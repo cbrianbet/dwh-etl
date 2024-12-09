5756cb8bf23f70ae8cbe992acef7c52be04c9dac
Mugo <dennis.ndwiga@thepalladiumgroup.com>
2024-12-09 20:51:21 +0300

    Replaced the default dbo schema on all the cleaning scripts plus the lookup tables

--- Scripts/ODS/data_quality/CT_cleaning_scripts/clean_CT_contact_listing.sql
+++ Scripts/ODS/data_quality/CT_cleaning_scripts/clean_CT_contact_listing.sql
@@ -1,5 +1,5 @@
 -- clean ContactAge
-UPDATE [ODS].[DBO].[CT_ContactListing]
+UPDATE [ODS].[Care].[CT_ContactListing]
     SET ContactAge = 999
 WHERE ContactAge < 0 OR ContactAge > 120
 
@@ -7,7 +7,7 @@ GO
 
 
 -- clean ContactSex
-UPDATE [ODS].[DBO].[CT_ContactListing]
+UPDATE [ODS].[Care].[CT_ContactListing]
     SET ContactSex = CASE 
                         WHEN ContactSex = 'U' THEN 'Undefined'
                         WHEN ContactSex = 'M' THEN 'Male'
@@ -18,7 +18,7 @@ WHERE ContactSex IN ('U', 'M', 'F')
 GO
 
 -- clean RelationshipWithPatient
-UPDATE [ODS].[DBO].[CT_ContactListing]
+UPDATE [ODS].[Care].[CT_ContactListing]
     SET RelationshipWithPatient = CASE 
                                     WHEN RelationshipWithPatient IN ('Daughter', 'Son') THEN 'Child'
                                     WHEN RelationshipWithPatient = 'Co-wife' THEN 'Sexual Partner'
@@ -32,7 +32,7 @@ GO
 
 
 -- clean IPVScreeningOutcome
-UPDATE [ODS].[DBO].[CT_ContactListing]
+UPDATE [ODS].[Care].[CT_ContactListing]
     SET IPVScreeningOutcome = CASE 
                                 WHEN IPVScreeningOutcome = '0' THEN 'False'
                                 WHEN IPVScreeningOutcome = 'No' THEN 'False'
--- Scripts/ODS/data_quality/CT_cleaning_scripts/clean_CT_covid.sql
+++ Scripts/ODS/data_quality/CT_cleaning_scripts/clean_CT_covid.sql
@@ -1,19 +1,19 @@
 -- clean Covid19AssessmentDate
-UPDATE [ODS].[DBO].[CT_Covid]
+UPDATE [ODS].[Care].[CT_Covid]
     SET Covid19AssessmentDate = NULL
 WHERE Covid19AssessmentDate < CAST('1980-01-01' AS DATE) OR Covid19AssessmentDate > GETDATE()
 
 GO
 
 -- clean DateGivenFirstDose
-UPDATE [ODS].[DBO].[CT_Covid]
+UPDATE [ODS].[Care].[CT_Covid]
     SET DateGivenFirstDose = NULL
 WHERE DateGivenFirstDose < CAST('1980-01-01' AS DATE) OR DateGivenFirstDose > GETDATE()
 
 GO
 
 -- clean DateGivenSecondDose
-UPDATE [ODS].[DBO].[CT_Covid]
+UPDATE [ODS].[Care].[CT_Covid]
     SET DateGivenSecondDose = NULL
 WHERE DateGivenSecondDose < CAST('1980-01-01' AS DATE) OR DateGivenSecondDose > GETDATE()
 
