98ee76d3aaec68055a8d15bf26aea19fb9eb2512
Marymary-dev <marywambu@gmail.com>
2025-01-13 17:56:45 +0300

    Filtering out for risk scores After Jan 2024 when the ML model was fixed

--- Scripts/NDWH/C&T FACT TABLES/load_FactIITRiskScores.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactIITRiskScores.sql
@@ -1,6 +1,7 @@
 
 IF OBJECT_ID(N'[NDWH].[fact].[FactIITRiskScores]', N'U') IS NOT NULL 
 	DROP TABLE [NDWH].[fact].[FactIITRiskScores];
+    
 BEGIN
 
 
@@ -71,7 +72,7 @@ left join appointments_from_last_visit on appointments_from_last_visit.PatientPK
     and appointments_from_last_visit.SiteCode = risk_scores.SiteCode
 left join NDWH.Dim.DimDate as appointment on appointment.Date = appointments_from_last_visit.NextAppointment
 where rank = 1 and patient.voided = 0
-
+and   cast(RiskEvaluationDateKey as date)>'2024-01-31'
 alter table NDWH.dbo.FactIITRiskScores add primary key(FactKey)
 
 END
