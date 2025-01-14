56ec549d8ebccc2bc5f71cff5a50f329df99e136
Marymary-dev <marywambu@gmail.com>
2025-01-14 11:34:00 +0300

    Selecting the correct Evaluation datekey variable

--- Scripts/NDWH/C&T FACT TABLES/load_FactIITRiskScores.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactIITRiskScores.sql
@@ -72,7 +72,7 @@ left join appointments_from_last_visit on appointments_from_last_visit.PatientPK
     and appointments_from_last_visit.SiteCode = risk_scores.SiteCode
 left join NDWH.Dim.DimDate as appointment on appointment.Date = appointments_from_last_visit.NextAppointment
 where rank = 1 and patient.voided = 0
-and   cast(RiskEvaluationDateKey as date)>'2024-01-31'
+and   cast(evaluation.DateKey as date)>'2024-01-31'
 alter table NDWH.fact.FactIITRiskScores add primary key(FactKey)
 
 END
