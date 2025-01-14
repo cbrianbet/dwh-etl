0c3b596707e02ba59abccd69c688a92c372f1e7f
Marymary-dev <marywambu@gmail.com>
2025-01-14 15:36:32 +0300

    Changing schema names from dbo

--- Scripts/NDWH/C&T FACT TABLES/load_FactHistoricalVisits.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactHistoricalVisits.sql
@@ -1,6 +1,6 @@
 
-IF OBJECT_ID(N'[NDWH].[dbo].[FactHistoricalVisits]', N'U') IS NOT NULL 
-	DROP TABLE NDWH.[dbo].[FactHistoricalVisits];
+IF OBJECT_ID(N'[NDWH].[fact].[FactHistoricalVisits]', N'U') IS NOT NULL 
+	DROP TABLE NDWH.[fact].[FactHistoricalVisits];
 
 
 BEGIN	
@@ -9,7 +9,7 @@ with MFL_partner_agency_combination as (
 		distinct MFL_Code,
 		SDP,
 	    SDP_Agency as Agency 
-	from ODS.dbo.All_EMRSites 
+	from ODS.Care.All_EMRSites 
 ),
 UniqueVisits as (
 Select   row_number() OVER (PARTITION BY SiteCode,Patientpkhash, VisitDate ORDER BY VisitDate DESC) AS NUM,
@@ -19,7 +19,7 @@ Select   row_number() OVER (PARTITION BY SiteCode,Patientpkhash, VisitDate ORDER
 	WHOStage,
 	NextAppointmentDate,
 	voided
-	from ODS.dbo.CT_PatientVisits as visits
+	from ODS.Care.CT_PatientVisits as visits
 	
 )
 
@@ -34,21 +34,21 @@ select
     NextAppointmentDate.DateKey As NextAppointmentDateKey,
 	WHOStage,
 	cast(getdate() as date) as LoadDate
-into NDWH.dbo.FactHistoricalVisits
+into NDWH.fact.FactHistoricalVisits
 from UniqueVisits as  visits
-inner join ODS.dbo.CT_ARTPatients as art on art.PatientPKHash=visits.PatientPKHash and art.SiteCode=visits.SiteCode
-inner join NDWH.dbo.DimPatient as patient on visits.PatientPKHash = patient.PatientPKHash and visits.SiteCode = patient.SiteCode
-left join NDWH.dbo.DimFacility as facility on facility.MFLCode = visits.SiteCode
+inner join ODS.Care.CT_ARTPatients as art on art.PatientPKHash=visits.PatientPKHash and art.SiteCode=visits.SiteCode
+inner join NDWH.Dim.DimPatient as patient on visits.PatientPKHash = patient.PatientPKHash and visits.SiteCode = patient.SiteCode
+left join NDWH.Dim.DimFacility as facility on facility.MFLCode = visits.SiteCode
 left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code = visits.SiteCode
-left join NDWH.dbo.DimPartner as partner on partner.PartnerName = MFL_partner_agency_combination.SDP
-left join NDWH.dbo.DimAgency as agency on agency.AgencyName = MFL_partner_agency_combination.Agency
-left join NDWH.dbo.DimDate as VisitDate on VisitDate.Date=visits.VisitDate
-left join NDWH.dbo.DimDate as StartARTDate on StartARTDate.Date=art.StartARTDate
-left join NDWH.dbo.DimDate as NextAppointmentDate on NextAppointmentDate.Date=visits.NextAppointmentDate
+left join NDWH.Dim.DimPartner as partner on partner.PartnerName = MFL_partner_agency_combination.SDP
+left join NDWH.Dim.DimAgency as agency on agency.AgencyName = MFL_partner_agency_combination.Agency
+left join NDWH.Dim.DimDate as VisitDate on VisitDate.Date=visits.VisitDate
+left join NDWH.Dim.DimDate as StartARTDate on StartARTDate.Date=art.StartARTDate
+left join NDWH.Dim.DimDate as NextAppointmentDate on NextAppointmentDate.Date=visits.NextAppointmentDate
 WHERE Visits.voided =0 and Visits.NUM=1 and VisitDate >= EOMONTH(DATEADD(MONTH, -11, GETDATE())) 
 
 
-alter table NDWH.dbo.FactHistoricalVisits add primary key(FactKey);
+alter table NDWH.fact.FactHistoricalVisits add primary key(FactKey);
 END
 
 
