47cec5fb2024e56cfbc2cf2ef7481f5b3e3f9d06
Marymary-dev <marywambu@gmail.com>
2025-01-18 10:50:36 +0300

    Filtering out for Initial test type results

--- Scripts/REPORTING/load_linelist_trans_hts.sql
+++ Scripts/REPORTING/load_linelist_trans_hts.sql
@@ -44,18 +44,19 @@ SELECT DISTINCT
     pat.NUPI,
     CAST(GETDATE() AS DATE) AS LoadDate 
 INTO REPORTING.dbo.LineListTransHTS 
-FROM NDWH.Fact.FactHTSClientTests hts
-LEFT JOIN NDWH.Dim.DimFacility f on f.FacilityKey = hts.FacilityKey
-LEFT JOIN NDWH.Dim.DimAgency a on a.AgencyKey = hts.AgencyKey
-LEFT JOIN NDWH.Dim.DimPatient pat on pat.PatientKey = hts.PatientKey
-LEFT JOIN NDWH.Dim.DimAgeGroup age on age.AgeGroupKey=hts.AgeGroupKey
-LEFT JOIN NDWH.Dim.DimPartner p on p.PartnerKey = hts.PartnerKey
-LEFT JOIN NDWH.Fact.FactHTSClientLinkages link on link.PatientKey = hts.PatientKey
-LEFT JOIN NDWH.Dim.DimDate e on e.DateKey = DateEnrolledKey
-LEFT JOIN NDWH.Dim.DimDate d on d.DateKey = hts.DateTestedKey
-left join NDWH.Fact.FactHTSPartnerNotificationServices pns on pns.PatientKey=hts.PatientKey
+FROM NDWH.dbo.FactHTSClientTests hts
+LEFT JOIN NDWH.dbo.DimFacility f on f.FacilityKey = hts.FacilityKey
+LEFT JOIN NDWH.dbo.DimAgency a on a.AgencyKey = hts.AgencyKey
+LEFT JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = hts.PatientKey
+LEFT JOIN NDWH.dbo.DimAgeGroup age on age.AgeGroupKey=hts.AgeGroupKey
+LEFT JOIN NDWH.dbo.DimPartner p on p.PartnerKey = hts.PartnerKey
+LEFT JOIN NDWH.dbo.FactHTSClientLinkages link on link.PatientKey = hts.PatientKey
+LEFT JOIN NDWH.dbo.DimDate e on e.DateKey = DateEnrolledKey
+LEFT JOIN NDWH.dbo.DimDate d on d.DateKey = hts.DateTestedKey
+left join NDWH.dbo.FactHTSPartnerNotificationServices pns on pns.PatientKey=hts.PatientKey
 WHERE  ( DATEDIFF ( MONTH, DOB, d.Date ) > 18 AND DATEDIFF ( MONTH, DOB, d.Date ) <= 1500 )
 AND FinalTestResult IS NOT NULL 
 AND d.[Date] >= CAST ( '2015-01-01' AS DATE )
+AND TestType ='Initial Test'
 
 
