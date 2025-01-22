730072ecaba836502f0b4c067cb0fb0959e70f2e
Mugo <dennis.ndwiga@thepalladiumgroup.com>
2025-01-22 10:00:00 +0300

    Updated CsLinelistHistoricalViralLoad on branch HistoricalViralLoad from CsLinelistHistoricalViralLoad branch for constistency.

--- Scripts/REPORTING/HIVCaseSurveillance/Load_Cs_LinelistHistoricalViralLoad.sql
+++ Scripts/REPORTING/HIVCaseSurveillance/Load_Cs_LinelistHistoricalViralLoad.sql
@@ -22,14 +22,14 @@ BEGIN
 			,IsValidVL As VLValidity
 			,VLSup As VLSuppression
 			INTO [HIVCaseSurveillance].[dbo].[CsLinelistHistoricalViralLoad]
-		FROM ndwh.dbo.FactViralLoad_Hist FactViralLoad_Hist
-		LEFT OUTER JOIN [NDWH].[dbo].[DimFacility] Facility
+		FROM ndwh.Fact.FactViralLoad_Hist FactViralLoad_Hist
+		LEFT OUTER JOIN [NDWH].[Dim].[DimFacility] Facility
 		ON FactViralLoad_Hist.FacilityKey	= Facility.FacilityKey
-		LEFT OUTER JOIN [NDWH].[dbo].[DimPatient] Patient
+		LEFT OUTER JOIN [NDWH].[Dim].[DimPatient] Patient
 		on FactViralLoad_Hist.PatientKey = Patient.PatientKey
-			LEFT OUTER JOIN [NDWH].[dbo].[DimDate] [Date]
+			LEFT OUTER JOIN [NDWH].[Dim].[DimDate] [Date]
 		ON FactViralLoad_Hist.AsOfDate = [Date].[Date]
-	LEFT OUTER JOIN [NDWH].[dbo].[DimAgeGroup] [AgeGroup]
+	LEFT OUTER JOIN [NDWH].[Dim].[DimAgeGroup] [AgeGroup]
 		ON FactViralLoad_Hist.AgeGroupKey = [AgeGroup].AgeGroupKey
 				where  YEAR(DateConfirmedHIVPositiveKey) <= YEAR(GETDATE())
 		
