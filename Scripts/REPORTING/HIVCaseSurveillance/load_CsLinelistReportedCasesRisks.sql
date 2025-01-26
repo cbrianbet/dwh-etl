7b53a6d79f670a94f0c08d3dbdaa4cf28b440dc8
Mumo Ngungu <mumongungu@gmail.com>
2025-01-26 22:58:16 +0300

    adding cohort year month to the report

--- Scripts/REPORTING/HIVCaseSurveillance/load_CsLinelistReportedCasesRisks.sql
+++ Scripts/REPORTING/HIVCaseSurveillance/load_CsLinelistReportedCasesRisks.sql
@@ -1,100 +1,106 @@
+IF OBJECT_ID(N'[HIVCaseSurveillance].[dbo].[CsLinelistEnhancedAdherenceCounsellingIndicators]', N'U') IS NOT NULL
+    DROP TABLE [HIVCaseSurveillance].[dbo].[CsLinelistEnhancedAdherenceCounsellingIndicators];
 
-IF OBJECT_ID(N'[HIVCaseSurveillance].[dbo].[CsLinelistReportedCasesRisks]', N'U') IS NOT NULL 
-	DROP TABLE [HIVCaseSurveillance].[dbo].[CsLinelistReportedCasesRisks];
-    with confirmed_reported_cases_and_art as (
+BEGIN
+
+    with unsurpressed_as_of_one_year_ago as (
         select 
-                art.PatientKey,
-                patient.Gender,
-                art.AgeLastVisit,
-                art.FacilityKey,
-                PartnerKey,
-                AgencyKey,
-                eomonth(confirmed_date.Date) as CohortYearMonth,
+            PatientKey,
+            FacilityKey,
+            IsValidVL,
+            VLSup,
+            TestResult,
+            TestName,
+            cast(OrderedByDate as date) as LastOrderedByDate,
+            AsOfDate,
+            AgeAsOfDate
+        from NDWH.[Fact].[FactViralLoad_Historical]
+        where datediff(month, AsOfDate, eomonth(dateadd(mm,-1,getdate()))) = 12 
+            and IsValidVL = 1 
+            and VLSup = 0 
+    ),
+    vl_tests_within_three_months_from_as_of_date as (
+        select 
+            vl.PatientKey,
+            unsurpressed_as_of_one_year_ago.AsOfDate,
+            unsurpressed_as_of_one_year_ago.LastOrderedByDate,
+            vl_date.Date as RepeatOrderedByDate,
+            datediff(day, unsurpressed_as_of_one_year_ago.LastOrderedByDate, vl_date.[Date]) as diff_in_days,
+            row_number() over (partition by vl.PatientKey order by vl_date.Date asc) as rank,
+            vl.TestResult as RepeatVLResult
+        from NDWH.Fact.FactVLLastTwoYears as vl
+        left join unsurpressed_as_of_one_year_ago on unsurpressed_as_of_one_year_ago.PatientKey = vl.PatientKey
+        left join NDWH.Dim.DimDate as vl_date on vl_date.DateKey = vl.OrderedbyDateKey
+        where datediff(day, unsurpressed_as_of_one_year_ago.LastOrderedByDate, vl_date.Date) between 7 and 90 -- diff between one week & 3 months
+    ),
+    earliest_repeat_vl_within_three_months as (
+        select
+        PatientKey, 
+        case
+            when ISNUMERIC(RepeatVLResult) = 1 then
                 case 
-					when art_date.Date < confirmed_date.Date then confirmed_date.Date
-					else art_date.Date
-				end as StartARTDate,
-                DATEDIFF(year,patient.DOB,confirmed_date.Date) as AgeatDiagnosis
-            from NDWH.dbo.FACTART as art 
-            left join NDWH.dbo.DimPatient as patient on patient.PatientKey = art.PatientKey
-            left join NDWH.dbo.DimDate as confirmed_date on confirmed_date.DateKey = patient.DateConfirmedHIVPositiveKey
-            left join NDWH.dbo.DimDate as art_date on art_date.DateKey = art.StartARTDateKey
-    
+                    when try_cast(replace(RepeatVLResult,',','') AS float) >= 1000.00 then 'UNSUPPRESSED' 
+                    when try_cast(replace(RepeatVLResult,',','') as float) between 200.00 and 999.00  then 'High Risk LLV '
+                    when try_cast(replace(RepeatVLResult,',','') as float) between 50.00 and 199.00 then 'Low Risk LLV'
+                    when try_cast(replace(RepeatVLResult,',','') as float) < 50 then 'LDL'
+                end
+        else
+            case
+                when RepeatVLResult IN ('Undetectable', 'NOT DETECTED', '0 copies/ml', 'LDL', 'Less than Low Detectable Level') then 'LDL' 
+                else null 
+            end 
+        end as RepeatVLCategory
+        from vl_tests_within_three_months_from_as_of_date
+        where rank = 1
     ),
-    RiskFactors as (
-        Select 
-        PatientKey,
-        row_number() OVER (PARTITION BY Patientkey ORDER BY VisitDatekey DESC) AS NUM,
-        case when MultiplePartners= 'YES' Then 1 Else 0 End as HasMultiplePartners,
-        case when NumberOfPartners is not null then NumberOfPartners Else 0 End as NumerofSexualPartners,
-        case when HIVRiskCategory in ('High','Moderate','Very high') Then 1 Else 0 End as HTSHighRiskCategory
-        from NDWH.dbo.FactHTSEligibilityextract as eligibility
-        where  PatientKey is not null 
-    ),
-    LatestRiskFactors as (
-        Select 
-        PatientKey,
-        HasMultiplePartners,
-        NumerofSexualPartners,
-        HTSHighRiskCategory
-        from RiskFactors
-        where NUM=1
-    ),
-    PrepVisits as (
-        SELECT
-        PatientKey,
-          row_number() OVER (PARTITION BY Patientkey ORDER BY VisitDatekey DESC) AS NUM
-        from NDWH.dbo.FactPrepVisits
-    ),
-    LatestPrepVisits as (
-        Select 
-        PatientKey
-        from PrepVisits
-        where NUM=1
-    ),
-    PBFWNotOnART as (
-        SELECT
-        Patientkey,
-        startregimen,
-        startartdatekey
-        from NDWH.dbo.FactART
-        where ispbfwatconfirmationpositive=1 and (startartdatekey is null and startregimen is null) 
-    ) , 
-    InfantsNotOnProphylaxis as (
-      SELECT
-      PatientKey
-      from ndwh.dbo.FactHEI
-      where onprohylaxis <> 1
-    )
+    eac_session_after_last_unsurpressed_vl as (
+        select 
+            eac.PatientKey,
+            unsurpressed_as_of_one_year_ago.LastOrderedByDate,
+            eac_date.Date as eac_date
+        from NDWH.Fact.FactEACVisitsLastTwoYears as eac
+        left join NDWH.Dim.DimDate as eac_date on eac_date.DateKey = eac.VisitDateKey
+        left join unsurpressed_as_of_one_year_ago on unsurpressed_as_of_one_year_ago.PatientKey = eac.PatientKey
+        where datediff(day, unsurpressed_as_of_one_year_ago.LastOrderedByDate, eac_date.Date) between 1 and 180 -- diff within 6 months
+        
+    ), 
+    patients_at_least_one_eac_session_after_last_unsurpressed_vl as (
     select 
-        confirmed_reported_cases_and_art.PatientKey,
-        Gender,
-        AgeLastVisit,
-        FacilityName,
-        PartnerName,
-        AgencyName,
-        CohortYearMonth,
-        StartARTDate,
-        HasMultiplePartners,
-        NumerofSexualPartners,
-        HTSHighRiskCategory,
-        case when LatestPrepVisits.PatientKey is not null then 1 else 0 End as Secoronverted,
-        case when AgeatDiagnosis <15 Then 1 Else 0 End as IsChild,
-        case when PBFWNotOnART.patientkey is not null then 1 Else 0 end as PbfwNotOnART,
-        case when InfantsNotOnProphylaxis.patientkey is not null then 1 else 0 End as InfantsNotOnProphylaxis
- into [HIVCaseSurveillance].[dbo].[CsLinelistReportedCasesRisks]
-    from confirmed_reported_cases_and_art
-    left join LatestRiskFactors as risks   on confirmed_reported_cases_and_art.PatientKey = risks.PatientKey
-    left join NDWH.dbo.DimFacility as facility on facility.FacilityKey=confirmed_reported_cases_and_art.FacilityKey
-    left join NDWH.dbo.DimPartner as partner on partner.PartnerKey=confirmed_reported_cases_and_art.PartnerKey
-    left join NDWH.dbo.DimAgency as agency on agency.AgencyKey=confirmed_reported_cases_and_art.AgencyKey
-    left join LatestPrepVisits on LatestPrepVisits.PatientKey=confirmed_reported_cases_and_art.PatientKey
-    left join PBFWNotOnART on PBFWNotOnART.Patientkey=confirmed_reported_cases_and_art.Patientkey
-    left join InfantsNotOnProphylaxis on InfantsNotOnProphylaxis.patientkey=confirmed_reported_cases_and_art.PatientKey
-   
-    
-  
-    
-
-
-
+        distinct PatientKey
+    from eac_session_after_last_unsurpressed_vl
+    ),
+    metrics as (
+    select 
+        unsurpressed_as_of_one_year_ago.PatientKey,
+        unsurpressed_as_of_one_year_ago.FacilityKey,
+        unsurpressed_as_of_one_year_ago.AgeAsOfDate,
+        case when unsurpressed_as_of_one_year_ago.PatientKey is not null then 1 else 0 end as IsUnsurpressedAsOfOneYearAgo,
+        case when earliest_repeat_vl_within_three_months.PatientKey is null then 1 else 0 end as WithoutRepeatVLWithinThreeMonths,
+        earliest_repeat_vl_within_three_months.RepeatVLCategory as RepeatVLCategory,
+        case when patients_at_least_one_eac_session_after_last_unsurpressed_vl.PatientKey is null then 1 else 0 end as WithoutEACSessionAfterLastUnsurpressedVL,
+        case when earliest_repeat_vl_within_three_months.Patientkey is not null and earliest_repeat_vl_within_three_months.RepeatVLCategory not in ('LDL', 'Low Risk LLV') then 1 else 0 end as IsSuspectedTreatmentFailure
+    from unsurpressed_as_of_one_year_ago
+    left join earliest_repeat_vl_within_three_months on unsurpressed_as_of_one_year_ago.PatientKey = earliest_repeat_vl_within_three_months.PatientKey
+    left join patients_at_least_one_eac_session_after_last_unsurpressed_vl on patients_at_least_one_eac_session_after_last_unsurpressed_vl.PatientKey = unsurpressed_as_of_one_year_ago.PatientKey
+    )
+    select
+        eomonth(dim_date.Date) as CohortYearMonth,
+        patient.PatientPKhash,
+        patient.Gender,
+        age_group.DATIMAgegroup as AgeGroup,
+        facility.FacilityName,
+        facility.County,
+        facility.SubCounty,
+        facility.MFLCode,
+        IsunsurpressedAsOfOneYearAgo,
+        WithoutRepeatVLWithinThreeMonths,
+        RepeatVLCategory,
+        WithoutEACSessionAfterLastUnsurpressedVL,
+        IsSuspectedTreatmentFailure
+    into HIVCaseSurveillance.dbo.CsLinelistEnhancedAdherenceCounsellingIndicators
+    from metrics
+    left join NDWH.Dim.DimPatient as patient on patient.Patientkey = metrics.Patientkey
+    left join NDWH.Dim.DimFacility as facility on facility.FacilityKey = metrics.FacilityKey
+    left join NDWH.Dim.DimAgeGroup as age_group on age_group.Age = metrics.AgeAsOfDate
+    left join NDWH.Dim.DimDate as dim_date on dim_date.DateKey = patient.DateConfirmedHIVPositiveKey
+END
