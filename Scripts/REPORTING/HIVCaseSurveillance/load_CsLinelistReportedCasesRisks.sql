72e9b9ddb56c8a4e83f3cd41e8dfe96587bebefc
Mumo Ngungu <mumongungu@gmail.com>
2025-01-29 00:00:53 +0300

    adding new aggregate table & adapting exisitng scripts

--- Scripts/REPORTING/HIVCaseSurveillance/load_CsLinelistReportedCasesRisks.sql
+++ Scripts/REPORTING/HIVCaseSurveillance/load_CsLinelistReportedCasesRisks.sql
@@ -18,21 +18,15 @@ IF OBJECT_ID(N'[HIVCaseSurveillance].[dbo].[CsLinelistReportedCasesRisks]', N'U'
 				end as StartARTDate,
 
                 DATEDIFF(year,patient.DOB,confirmed_date.Date) as AgeatDiagnosis,
-                age.DATIMAgeGroup as AgeGroup
+                agegroup.DATIMAgeGroup as AgeGroup
 
 
             from NDWH.Fact.FACTART as art 
             left join NDWH.Dim.DimPatient as patient on patient.PatientKey = art.PatientKey
             left join NDWH.Dim.DimDate as confirmed_date on confirmed_date.DateKey = patient.DateConfirmedHIVPositiveKey
             left join NDWH.Dim.DimDate as art_date on art_date.DateKey = art.StartARTDateKey
-<<<<<<< HEAD
             left join NDWH.Dim.DimDate as prep_enrol_date on prep_enrol_date.DateKey = patient.PrepEnrollmentDateKey
             left join NDWH.Dim.DimAgeGroup as agegroup on agegroup.AgeGroupKey=art.AgeGroupKey  
-=======
-            left join NDWH.Dim.DimAgeGroup as age on age.AgeGroupKey=art.AgeGroupKey
-
-    
->>>>>>> de73e05b8c1e8064e51906ed22efea464151a267
     ),
     RiskFactors as (
         Select 
@@ -80,8 +74,8 @@ IF OBJECT_ID(N'[HIVCaseSurveillance].[dbo].[CsLinelistReportedCasesRisks]', N'U'
         HasMultiplePartners,
         NumerofSexualPartners,
         HTSHighRiskCategory,
-        case when confirmed_date.Date is not null and PrepEnrollmentDatekey is not null then 1 else 0 End as Seroconverted,
-        case when AgeatDiagnosis <15 Then 1 Else 0 End as IsChild,
+        case when confirmed_reported_cases_and_art.CohortYearMonth is not null and confirmed_reported_cases_and_art.PrepEnrollmentDate is not null then 1 else 0 End as Seroconverted,
+        case when AgeatDiagnosis < 15 Then 1 Else 0 End as IsChild,
         case when PBFWNotOnART.patientkey is not null then 1 Else 0 end as PbfwNotOnART,
         case when InfantsNotOnProphylaxis.patientkey is not null then 1 else 0 End as InfantsNotOnProphylaxis
     into [HIVCaseSurveillance].[dbo].[CsLinelistReportedCasesRisks]
@@ -90,6 +84,5 @@ IF OBJECT_ID(N'[HIVCaseSurveillance].[dbo].[CsLinelistReportedCasesRisks]', N'U'
     left join NDWH.Dim.DimFacility as facility on facility.FacilityKey=confirmed_reported_cases_and_art.FacilityKey
     left join NDWH.Dim.DimPartner as partner on partner.PartnerKey=confirmed_reported_cases_and_art.PartnerKey
     left join NDWH.Dim.DimAgency as agency on agency.AgencyKey=confirmed_reported_cases_and_art.AgencyKey
-    left join LatestPrepVisits on LatestPrepVisits.PatientKey=confirmed_reported_cases_and_art.PatientKey
     left join PBFWNotOnART on PBFWNotOnART.Patientkey=confirmed_reported_cases_and_art.Patientkey
     left join InfantsNotOnProphylaxis on InfantsNotOnProphylaxis.patientkey=confirmed_reported_cases_and_art.PatientKey
---
+++ Scripts/REPORTING/HIVCaseSurveillance/load_cs_aggregate_tested_neg_prep_start.sql
@@ -0,0 +1,64 @@
+if object_id(N'[HIVCaseSurveillanceEPORTING].[dbo].[CSAggregateTestedNegativePrepStart]', N'U') is not null 			
+	drop table [HIVCaseSurveillance].[dbo].[CSAggregateTestedNegativePrepStart]
+go
+
+begin
+
+    with source_data as (
+        select 
+            row_number() over (partition by tests.FacilityKey, tests.PatientKey, tests.TestType order by tests.DateTestedKey desc) as num,
+            elig.VisitDateKey,
+            elig.HIVRiskCategory,
+            elig.HtsRiskScore,
+            case 
+                when patient.PrepEnrollmentDateKey is not null and datediff(month, testDate.Date, prepStart.Date) <= 3 then 1 else 0 end as IsStartedOnPrep,
+            patient.PatientKey,
+            agegroup.DATIMAgeGroup,
+            patient.Gender,
+            facility.FacilityName,
+            facility.latitude,
+            facility.longitude,
+            facility.County,
+            facility.SubCounty,
+            facility.MFLCode,
+            partner.PartnerName,
+            agency.AgencyName
+        from NDWH.Fact.FactHTSClientTests as tests
+        left join NDWH.Fact.FactHTSEligibilityExtract elig on elig.PatientKey = tests.PatientKey
+            and elig.VisitDateKey = tests.DateTestedKey
+        left join NDWH.Dim.DimFacility as facility on facility.FacilityKey = tests.FacilityKey
+        left join NDWH.Dim.DimAgency as agency on agency.AgencyKey = tests.AgencyKey
+        left join NDWH.Dim.DimPartner as partner on partner.PartnerKey = tests.PartnerKey
+        left join NDWH.Dim.DimPatient as patient on patient.PatientKey = tests.PatientKey
+        left join NDWH.Dim.DimDate as testDate on testDate.DateKey =tests.DateTestedKey
+        left join NDWH.Dim.DimDate as prepStart on prepStart.DateKey = patient.PrepEnrollmentDateKey
+        left join NDWH.Dim.DimAgeGroup as agegroup on agegroup.AgeGroupKey = tests.AgeGroupKey
+        where 
+        tests.TestType = 'Initial Test' 
+        and HIVRiskCategory is not null
+        and agegroup.Age >= 15 --only picking for 15+ year olds who are eligible for PrEP
+    )
+    select
+        DATIMAgeGroup as AgeGroup,
+        Gender,
+        FacilityName,
+        County,
+        SubCounty,
+        PartnerName,
+        AgencyName,
+        HIVRiskCategory,
+        count(*) as TestedNegative,
+        sum(IsStartedOnPrep) as StartedOnPrep
+    into [HIVCaseSurveillance].[dbo].[CSAggregateTestedNegativePrepStart]
+    from source_data
+    group by 
+        DATIMAgeGroup,
+        Gender,
+        FacilityName,
+        County,
+        SubCounty,
+        PartnerName,
+        AgencyName,
+        HIVRiskCategory
+        
+end
