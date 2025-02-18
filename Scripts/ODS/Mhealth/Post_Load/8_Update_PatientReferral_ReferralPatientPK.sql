c73b3ad661dc9340e104760367b1bc136a2495eb
Mugo <dennis.ndwiga@thepalladiumgroup.com>
2025-02-18 11:05:57 +0300

    Deleted the post_load folder since its not required in the etl

--- Scripts/ODS/Mhealth/Post_Load/8_Update_PatientReferral_ReferralPatientPK.sql
+++
@@ -1,8 +0,0 @@
-/* Update of the ReferralpatientPK from the C&T patients.This are for the patients who are in ushauri and have been
-registered in the EMR. Indentified using the patientCCCNumber */
-
-UPDATE a
-    SET a.ReferralpatientPK = p.PatientPK
-FROM [ODS].[Mhealth].[Mhealth_FacilityReferral_Patient] a
-    JOIN [ODS].[Mhealth].[CT_Patient] p
-ON  a.PatientID = p.PatientID;
