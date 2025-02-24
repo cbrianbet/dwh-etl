0a688618bf330ab34a21c4e66a32bb6f019ca501
cbrianbet <cbrianbet@outlook.com>
2025-02-24 09:30:53 +0300

    fixed issues in AggregateFACT_CT_DHIS2
    - fixed issues in AggregateFACT_CT_DHIS2
    - improved readability in AggregateFACT_HTS_DHIS2

---
+++ Scripts/REPORTING/load_aggregate_ct_dhis2.sql
@@ -0,0 +1,58 @@
+IF OBJECT_ID(N'[REPORTING].[dbo].[AggregateFACT_CT_DHIS2]', N'U') IS NOT NULL
+	DROP TABLE [REPORTING].[dbo].[AggregateFACT_CT_DHIS2];
+BEGIN
+	with dhis as (
+		SELECT
+			*
+		FROM NDWH.Fact.FACT_CT_DHIS2 CT
+	)
+
+	SELECT
+		DHISOrgId
+		, MFLCode
+        , FacilityName
+        , County
+        , SubCounty
+        , AgencyName
+        , PartnerName
+        , Ward
+        , ReportMonth_Year
+        , Enrolled_Total
+        , StartedART_Total
+        , CurrentOnART_Total
+        , CTX_Total
+        , OnART_12Months
+        , NetCohort_12Months
+        , VLSuppression_12Months
+        , VLResultAvail_12Months
+        , createdAt
+        , updatedAt
+        , Start_ART_Under_1
+        , Start_ART_1_9
+        , Start_ART_10_14_M
+        , Start_ART_10_14_F
+        , Start_ART_15_19_M
+        , Start_ART_15_19_F
+        , Start_ART_20_24_M
+        , Start_ART_20_24_F
+        , Start_ART_25_Plus_M
+        , Start_ART_25_Plus_F
+        , On_ART_Under_1
+        , On_ART_1_9
+        , On_ART_10_14_M
+        , On_ART_10_14_F
+        , On_ART_15_19_M
+        , On_ART_15_19_F
+        , On_ART_20_24_M
+        , On_ART_20_24_F
+        , On_ART_25_Plus_M
+        , On_ART_25_Plus_F
+        , cast(getdate() as date) as LoadDate
+	INTO REPORTING.dbo.AggregateFACT_CT_DHIS2
+	FROM dhis
+		LEFT JOIN NDWH.Dim.DimFacility fac on dhis.FacilityKey=fac.FacilityKey
+		LEFT JOIN NDWH.Dim.DimAgency agency on dhis.AgencyKey=agency.AgencyKey
+		LEFT JOIN NDWH.Dim.DimPartner part on dhis.PartnerKey=part.PartnerKey
+	WHERE PartnerName IS NOT NULL
+
+END
--- Scripts/REPORTING/load_aggregate_hts_dhis2.sql
+++ Scripts/REPORTING/load_aggregate_hts_dhis2.sql
@@ -1,44 +1,48 @@
-IF OBJECT_ID(N'[REPORTING].[dbo].[AggregateFACT_HTS_DHIS2]', N'U') IS NOT NULL 
+IF OBJECT_ID(N'[REPORTING].[dbo].[AggregateFACT_HTS_DHIS2]', N'U') IS NOT NULL
 	DROP TABLE [REPORTING].[dbo].[AggregateFACT_HTS_DHIS2];
-Begin
-	SELECT 	
-			[MFLCode]
-			,[facility].FacilityName
-			,facility.County
-			,facility.SubCounty
-			,[ReportMonth_Year]
-			,[Tested_Total]
-			,[Positive_Total]
-			,[createdAt]
-			,[updatedAt]
-			,[Tested_1_9]
-			,[Tested_10_14_M]
-			,[Tested_10_14_F]
-			,[Tested_15_19_M]
-			,[Tested_15_19_F]
-			,[Tested_20_24_M]
-			,[Tested_20_24_F]
-			,[Tested_25_Plus_M]
-			,[Tested_25_Plus_F]
-			,[Positive_1_9]
-			,[Positive_10_14_M]
-			,[Positive_10_14_F]
-			,[Positive_15_19_M]
-			,[Positive_15_19_F]
-			,[Positive_20_24_M]
-			,[Positive_20_24_F]
-			,[Positive_25_Plus_M]
-			,[Positive_25_Plus_F]
-			, partner.PartnerName
-			,Agency as AgencyName
-			,CAST(GETDATE() AS DATE) AS LoadDate
+BEGIN
+	with dhis as (
+		SELECT
+			*
+		FROM NDWH.Fact.FACT_HTS_DHIS2
+	)
+
+	SELECT
+		MFLCode
+        , FacilityName
+        , County
+        , SubCounty
+        , agency.AgencyName
+        , part.PartnerName
+        , [ReportMonth_Year]
+        , [Tested_Total]
+        , [Positive_Total]
+        , [createdAt]
+        , [updatedAt]
+        , [Tested_1_9]
+        , [Tested_10_14_M]
+        , [Tested_10_14_F]
+        , [Tested_15_19_M]
+        , [Tested_15_19_F]
+        , [Tested_20_24_M]
+        , [Tested_20_24_F]
+        , [Tested_25_Plus_M]
+        , [Tested_25_Plus_F]
+        , [Positive_1_9]
+        , [Positive_10_14_M]
+        , [Positive_10_14_F]
+        , [Positive_15_19_M]
+        , [Positive_15_19_F]
+        , [Positive_20_24_M]
+        , [Positive_20_24_F]
+        , [Positive_25_Plus_M]
+        , [Positive_25_Plus_F]
+        , cast(getdate() as date) as LoadDate
 	INTO REPORTING.dbo.AggregateFACT_HTS_DHIS2
-		FROM NDWH.Fact.FACT_HTS_DHIS2 CT  
-		LEFT join NDWH.Dim.DimFacility facility on facility.FacilityKey=CT.facilitykey 
-        left join NDWH.Dim.DimPartner partner on partner.PartnerKey=CT.PartnerKey
-        left join NDWH.Dim.DimAgency agency on agency.AgencyKey=CT.Agencykey
-        WHERE MFLCode IS NOT NULL
-	
-End
+	FROM dhis
+		LEFT JOIN NDWH.Dim.DimFacility fac on dhis.FacilityKey=fac.FacilityKey
+		LEFT JOIN NDWH.Dim.DimAgency agency on dhis.AgencyKey=agency.AgencyKey
+		LEFT JOIN NDWH.Dim.DimPartner part on dhis.PartnerKey=part.PartnerKey
+	WHERE MFLCode IS NOT NULL
 
-
+END
